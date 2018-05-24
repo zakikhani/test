ed19da8d288838010e4207b66f5b0586ff6ec952
Arash Zakikhani <az334@georgetown.edu>
2018-05-24 15:42:14 -0400

    initial project version

---
+++ RecordedScript.py
@@ -0,0 +1,15 @@
+﻿# $language = "python"
+# $interface = "1.0"
+
+# This automatically generated script may need to be
+# edited in order to work correctly.
+
+def Main():
+	crt.Screen.Synchronous = True
+	crt.Screen.WaitForString("HAR-BDF-C651>")
+	crt.Screen.Send(chr(13))
+	crt.Screen.Send("en" + chr(13))
+	crt.Screen.WaitForString("password: ")
+	crt.Screen.Send("Jigargeo1@" + chr(13))
+
+Main()
